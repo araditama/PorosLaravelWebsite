c1c174feae6e474f08def07600d8819db15e9285
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2018-08-05 17:53:15 +0700

    Voyager Library Init

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ Route::get('/', 'HomeController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
